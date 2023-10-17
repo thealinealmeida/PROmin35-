c0eafadce1ae93cb42d44a814d607deb51f4bd6e
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-10-17 17:17:21 -0300

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,17 +1,1 @@
-class Ball
-    {
-        constructor (x,y,r,newColor)
-    {
-       this.x = x;
-        this.y = y;
-        this.radius = r;
-        this.color = newColor;
-    }
-        
-     display()   
-    {
-       fill(this.color);    
-       ellipse(this.x,this.y,this.radius,this.radius) ;
-        
-    }
-}
+
