85643d814b5feb775810d176e0215d959ff495a1
thealinealmeida <68761501+thealinealmeida@users.noreply.github.com>
2023-10-17 17:17:37 -0300

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,16 +1,1 @@
-class Box
-    {
-      constructor(x,y,w,h,newColor){
-          this.x = x;
-          this.y = y;
-          this.width = w;
-          this.height = h;
-          this.color = newColor;
-      }    
-      display(){
-          fill(this.color);
-          rect(this.x, this.y, this.width,this.height);
-      }
-        
-    }
 
